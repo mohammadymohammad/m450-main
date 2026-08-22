d27d5b9dc97863e3a6fbad343818d00675dc2f2e
mohammadymohammad <mohammad.mohammady@edu.gbssg.ch>
2026-08-22 20:16:32 +0200

    7 Tests were written and run successfully...

--- tests/M450.UnitTesting.App.Tests/PriceCalculatorTests.cs
+++ tests/M450.UnitTesting.App.Tests/PriceCalculatorTests.cs
@@ -2,9 +2,116 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace M450.UnitTesting.App.Tests
+namespace M450.UnitTesting.App.Tests;
+
+public class PriceCalculatorTests
 {
-    internal class PriceCalculatorTests
+    [Fact]
+    void CalculateTotal_WithDiscount_ReturnsExpectedTotal()
     {
+        // Arrange
+        PriceCalculator calculator = new PriceCalculator();
+        decimal unitPrice = 10.0m;
+        int quantity = 2;
+        decimal discount = 25m;
+        decimal expected = 15.0m;
+
+        // Act
+        decimal total = calculator.CalculateTotal(unitPrice, quantity, discount);
+
+        // Assert
+        Assert.Equal(expected, total);
+    }
+
+    [Fact]
+    void CalculateTotal_WithoutDiscount_ReturnExpectedTotal()
+    {
+        //Arrange
+        PriceCalculator calculator = new PriceCalculator();
+        decimal unitPrice = 5.0m;
+        int quantity = 3;
+        decimal expected = 15.0m;
+
+        //Act
+        decimal total = calculator.CalculateTotal(unitPrice, quantity);
+
+        //Assert
+        Assert.Equal(expected, total);
+    }
+
+    [Fact]
+    void CalculateTotal_WithZeroQuantity_ReturnZeroAsTotal()
+    {
+        //Arrange
+        PriceCalculator calculator = new PriceCalculator();
+        decimal unitPrice = 5.0m;
+        int quantity = 0;
+        decimal discount = 25m;
+        decimal expected = 0m;
+
+        //Act
+        decimal total = calculator.CalculateTotal(unitPrice, quantity, discount);
+
+        //Assert
+        Assert.Equal(expected, total);
+    }
+
+    [Fact]
+    void CalculateTotal_With100Discount_ReturnZeroAsTotal()
+    {
+        //Arrange
+        PriceCalculator calculator = new PriceCalculator();
+        decimal unitPrice = 15.0m;
+        int quantity = 3;
+        decimal discount = 100m;
+        decimal expected = 0m;
+
+        //Act
+        decimal total = calculator.CalculateTotal(unitPrice, quantity, discount);
+
+        //Assert
+        Assert.Equal(expected, total);
+    }
+
+    [Fact]
+    void CalculateTotal_WithNigativeUnitPrice_ThrowsArgumentOutOfRangeException()
+    {
+        //Arrange
+        PriceCalculator calculator = new PriceCalculator();
+        decimal unitPrice = -15.0m;
+        int quantity = 3;
+        decimal discount = 100m;
+
+        //Act & Assert
+        Assert.Throws<ArgumentOutOfRangeException>(
+            () => calculator.CalculateTotal(unitPrice, quantity, discount) );
+    }
+
+    [Fact]
+    void CalculateTotal_WithNegativeQuantity_ThrowsArgumentOutOfRangeException()
+    {
+        //Arrange
+        PriceCalculator calculator = new PriceCalculator();
+        decimal unitPrice = 15.0m;
+        int quantity = -3;
+        decimal discount = 100m;
+
+        //Act & Assert
+        Assert.Throws<ArgumentOutOfRangeException>(
+            () => calculator.CalculateTotal(unitPrice, quantity, discount));
+    }
+
+    [Fact]
+    void CalculateTotal_WithInvalidDiscount_ThrowsArgumentOutOfRangeException()
+    {
+        //Arrange
+        PriceCalculator calculator = new PriceCalculator();
+        decimal unitPrice = 15.0m;
+        int quantity = 3;
+        decimal discount = 101m; // Disount can't be greater than 100m.
+
+        //Act & Assert
+        Assert.Throws<ArgumentOutOfRangeException>(
+            () => calculator.CalculateTotal(unitPrice, quantity, discount));
     }
 }
